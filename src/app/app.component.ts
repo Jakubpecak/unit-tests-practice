0963e9189f82499f2fec1ccc6b127de007d53fcc
Jakubpecak <kanaknakola021@gmail.com>
2024-02-05 20:55:52 +0100

    add tailwind css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-root',
@@ -11,4 +12,10 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'unit-tests';
+
+  constructor(private toastr: ToastrService) {}
+
+  showSuccess() {
+    this.toastr.success('Success message', 'Success');
+  }
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,12 @@
-import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { importProvidersFrom } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToastrModule } from 'ngx-toastr';
+import { HttpClientModule } from '@angular/common/http';
 
-import { routes } from './app.routes';
-
-export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
-};
+export const appConfig = [
+  importProvidersFrom(
+    BrowserAnimationsModule,
+    ToastrModule.forRoot(),
+    HttpClientModule
+  ),
+];
